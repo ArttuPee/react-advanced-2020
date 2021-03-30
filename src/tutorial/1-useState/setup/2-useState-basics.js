ea09e0a4a54b7c4320c6286ce8a9c39f8ad54b26
Arttu <arttu.pyykonen105@gmail.com>
2021-03-30 13:34:59 +0300

    useState tutoriaali valmis

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log(useState("hello world"));
+  // const value = useState(1)[0];
+  // const handler = useState(1)[1];
+  // console.log(value, handler);
+  const [text, setText] = useState("mikä olet tänään?");
+
+  const handleClick = () => {
+    if (text === "mikä olet tänään?" || text === 'mestari') {
+      setText("kuningas");
+    } else {
+      setText("mestari");
+    }
+  };
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button className="btn" onClick={handleClick}>
+        selvitä mikä olet
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
